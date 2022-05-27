a2b8defe99d3c84b45d0992fe14424eac2b162ec
Luiz Vinícius <azevedolvinicius@hotmail.com>
2022-05-27 20:47:50 -0300

    lista até a 8

---
+++ modulo4/lista-typescript/src/exercicio10.ts
@@ -0,0 +1,13 @@
+// function validaCPF(cpf:string):any{
+//     const arrayCPF = cpf.split("-")
+//     const codigoBase = arrayCPF[0].split(".")
+//     for (let i = 0;i < codigoBase.length; i++){
+//         codigoBase.map((numero)=>{
+//             return [numero[i]]
+//         })
+//     }
+// }
+// console.log(validaCPF("164.041.37-61"));
+// // let somaDebitos = cliente.debitos.reduce((acumulador:number, item) => {
+// //     acumulador += item
+// //     return acumulador
